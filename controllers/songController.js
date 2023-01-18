0d9a0e5f3bd8af2bcc46f6df4d0a3068fa860957
NRiddick113 <NatashaRiddick@pursuit.org>
2023-01-18 12:54:51 -0500

    part 2

--- controllers/songController.js
+++ controllers/songController.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const songs = express.Router();
-const { getAllSongs } = require("../queries/songs");
+const { getAllSongs, getSong, createSong} = require("../queries/songs");
+const { checkBoolean, checkName, checkArtist } = require("../validations/checkBookmarks.js");
 
 // INDEX
 songs.get("/", async (req, res) => {
@@ -11,4 +12,25 @@ songs.get("/", async (req, res) => {
       res.status(500).json({ error: "server error" });
     }
   });
+
+   // SHOW
+songs.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const song = await getSong(id);
+  if (song) {
+    res.json(song);
+  } else {
+    res.status(404).json({ error: "not found" });
+  }
+});
+
+// CREATE
+songs.post("/", checkBoolean, checkName, checkArtist, async (req, res) => {
+  try {
+    const song = await createSong(req.body);
+    res.json(song);
+  } catch (error) {
+    res.status(400).json({ error: error });
+  }
+});
 module.exports = songs;
